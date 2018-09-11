e7dcb3e8c596a55bb69fc5f7fda7b97d2dbfe6eb
Russell Melick <russell.melick@kiwigrid.com>
2018-09-10 20:53:02 -0700

    set for list of players in game

--- src/main/java/net/rmelick/hanabi/backend/GameWaitingToBegin.java
+++ src/main/java/net/rmelick/hanabi/backend/GameWaitingToBegin.java
@@ -2,7 +2,11 @@ package net.rmelick.hanabi.backend;
 
 
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
 import java.util.UUID;
 
 /**
@@ -10,7 +14,7 @@ import java.util.UUID;
  */
 public class GameWaitingToBegin {
   private final String _gameId = UUID.randomUUID().toString();
-  private final List<PlayerInfo> _players = new ArrayList<>();
+  private final Set<PlayerInfo> _players = new TreeSet<>();
 
   public void addPlayer(PlayerInfo playerInfo) {
     _players.add(playerInfo);
@@ -21,6 +25,6 @@ public class GameWaitingToBegin {
   }
 
   public List<PlayerInfo> getPlayers() {
-    return _players;
+    return new ArrayList<>(_players);
   }
 }
--- src/main/java/net/rmelick/hanabi/backend/PlayerInfo.java
+++ src/main/java/net/rmelick/hanabi/backend/PlayerInfo.java
@@ -1,9 +1,11 @@
 package net.rmelick.hanabi.backend;
 
+import java.util.Objects;
+
 /**
  *
  */
-public class PlayerInfo {
+public class PlayerInfo implements Comparable<PlayerInfo>{
   private String _name;
   private String _id;
   private PlayerType _type;
@@ -24,5 +26,29 @@ public class PlayerInfo {
 
   public PlayerType getType() {
     return _type;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    PlayerInfo that = (PlayerInfo) o;
+    return Objects.equals(_name, that._name) &&
+        Objects.equals(_id, that._id) &&
+        _type == that._type;
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(_name, _id, _type);
+  }
+
+  @Override
+  public int compareTo(PlayerInfo o) {
+    return this.getId().compareTo(o.getId());
   }
 }
